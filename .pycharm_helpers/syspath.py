d15967c17fc3baf75fd6df3b00f1f9d3a9782a47
Jamie Wendle <jameswendle1@gmail.com>
2023-08-30 01:38:59 -0700

    08/30/2023

--- .pycharm_helpers/syspath.py
+++
@@ -1,8 +0,0 @@
-import os.path
-import sys
-
-_helpers_root = os.path.dirname(os.path.abspath(__file__))
-
-for root in sys.path:
-    if root != _helpers_root and root != os.curdir:
-        print(root)
